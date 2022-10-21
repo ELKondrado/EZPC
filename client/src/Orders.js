afff1503bee7bf336cb565e98317e4000fb96208
ELKondradoSefu <radu.condrat@student.upt.ro>
2022-10-21 19:11:46 +0300

    end

--- client/src/Orders.js
+++ client/src/Orders.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 
 function Orders() {
   const [tables,setTables] = useState([])
-  const [tablesN,setTablesN] = useState([])
+  let [tablesN,setTablesN] = useState([])
 
   function transferTableOrders()
   {
@@ -23,7 +23,10 @@ function Orders() {
           }
         });
 
-        console.log(v.length)
+        let cartItems = localStorage.getItem("productsInCart");
+        cartItems = JSON.parse(cartItems);
+        console.log(cartItems )
+
         for(let i=0;i<v.length;i++)
         {
            
@@ -31,82 +34,64 @@ function Orders() {
                 orderTableName : v[i]
             })
 
-            Axios.get("http://localhost:3001/api/getTableName",{params:{name:v[i]}}).then((response) => {
-                setTablesN(prevItems => [...prevItems,response.data]);
-                console.log(tablesN)
+            Axios.get("http://localhost:3001/api/getTableName",{params:{name:v[i]}}).then((response) => 
+            {
+
+                let orderContainer = document.querySelector(".orders-container");
+                let j=0;
+                Object.values(response.data).map((item) => 
+                {
+                  if(j==0)
+                  {
+                    j++;
+                    orderContainer.innerHTML += `
+                      <div className="tableHead">
+                        <p>Order ${i+1}</p>
+                        <div className="order-container">
+                          <table width="100%">
+                          <thead>
+                              <tr>
+                                  <td className="orderImage${i+1}">Image</td>
+                                  <td className="orderName${i+1}">Product</td>
+                                  <td className="orderPrice${i+1}">Price</td>
+                                  <td className="orderQuantity${i+1}">Quantity</td>
+                              </tr>
+                          </thead>
+                          <tbody className="products-to-cart">
+                            <td><img src=${item.productImage} alt=""></td>
+                            <td>${item.productName}</td>
+                            <td>$${item.productPrice}</td>
+                            <td>${item.productQuantity}</td>
+                          </tbody>
+                          </table>
+                        </div>
+                      </div>
+                      `;
+
+                  }
+                  else
+                  {
+                    orderContainer.innerHTML += `
+                      <div className="tableHead">
+                        <div className="order-container">
+                          <table width="100%">
+                          
+                          <tbody className="products-to-cart">
+                            <td><img src=${item.productImage} alt=""></td>
+                            <td>${item.productName}</td>
+                            <td>$${item.productPrice}</td>
+                            <td>${item.productQuantity}</td>
+                          </tbody>
+                          </table>
+                        </div>
+                      </div>
+                      `;
+                  }
+                });
             })
         }
-        
-        startOrderPage()
     }
   }
-
-  function startOrderPage(){
-    let orderContainer = document.querySelector(".orders-container");
-    let j=0;
-   
-    console.log(tablesN)
-    for(let i=0;i<tablesN.length;i++)
-    { 
-        Object.values(tablesN[i]).map((item) => 
-        {
-          if(i==j)
-          {
-            j++;
-            orderContainer.innerHTML += `
-              <div className="tableHead">
-                <p>Order ${i+1}</p>
-                <div className="order-container">
-                  <table width="100%">
-                  <thead>
-                      <tr>
-                          <td className="orderImage${i+1}">Image</td>
-                          <td className="orderName${i+1}">Product</td>
-                          <td className="orderPrice${i+1}">Price</td>
-                          <td className="orderQuantity${i+1}">Quantity</td>
-                      </tr>
-                  </thead>
-                  <tbody className="products-to-cart">
-                    <td><img src=${item.productImage} alt=""></td>
-                    <td>${item.productName}</td>
-                    <td>$${item.productPrice}</td>
-                    <td>$${item.productQuantity}</td>
-                  </tbody>
-                  </table>
-                </div>
-              </div>
-              `;
-
-          }
-          else
-          {
-            orderContainer.innerHTML += `
-              <div className="tableHead">
-                <div className="order-container">
-                  <table width="100%">
-                  <thead>
-                      <tr>
-                          <td className="orderImage${i+1}">Image</td>
-                          <td className="orderName${i+1}">Product</td>
-                          <td className="orderPrice${i+1}">Price</td>
-                          <td className="orderQuantity${i+1}">Quantity</td>
-                      </tr>
-                  </thead>
-                  <tbody className="products-to-cart">
-                    <td><img src=${item.productImage} alt=""></td>
-                    <td>${item.productName}</td>
-                    <td>$${item.productPrice}</td>
-                    <td>$${item.productQuantity}</td>
-                  </tbody>
-                  </table>
-                </div>
-              </div>
-              `;
-          }
-        });
-  }
-}
-
 
   return(
     <div className="Orders">
